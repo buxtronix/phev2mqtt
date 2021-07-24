9b172a6f71808970b061245cad8a5b7b827b66e9
Martin John <20387863+martingjohn@users.noreply.github.com>
2021-07-24 14:59:35 +0100

    Added check for Outlander on network, changed some config options, see CHANGELOG.md for more details

--- addon/run.sh
+++ addon/run.sh
@@ -5,8 +5,28 @@ CONFIG_PATH=/data/options.json
 export mqtt_server="$(bashio::config 'mqtt_server')"
 export mqtt_user="$(bashio::config 'mqtt_user')"
 export mqtt_password="$(bashio::config 'mqtt_password')"
+export debug="$(bashio::config 'debug')"
 
-/tmp/phev2mqtt/phev2mqtt \
+if [[ $debug == "true" ]]
+then
+	echo Debug mode on - sleeping indefinitely
+	sleep inf
+fi
+
+#Test to see if we can connect to the Outlander
+nc -zw 1 192.168.8.46 8080
+RC=$?
+
+if [[ $RC -ne  0 ]]
+then
+	echo Connection to Outlander unsuccessful
+	exit 1
+fi
+
+echo Connection to Outlander successful
+echo Starting phev2mqtt
+
+/opt/phev2mqtt \
         client \
         mqtt \
         --mqtt_server "tcp://${mqtt_server}/" \
