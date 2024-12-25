2b9a918d277f8f20b069438e4051a88dc58fe350
Simonas Kazlauskas <git@kazlauskas.me>
2024-12-25 15:34:51 +0200

    Ignore battery level readings that are <= 5%
    The vehicle keeps sending
    
    DEBU[0181] %PHEV_TCP_SEND_MSG%: [bd] REGISTER SET  (reg 0x06 data 03)
    DEBU[0181] %PHEV_TCP_RECV_MSG%: [bd] REGISTER SETACK (reg 0x06 data 00)
    DEBU[0181] %PHEV_TCP_RECV_MSG%: [2a] REGISTER NTFY (reg 0x12 data 180c190f230b03)
    DEBU[0181] %PHEV_TCP_SEND_MSG%: [2a] REGISTER ACK  (reg 0x12 data 00)
    DEBU[0181] %PHEV_TCP_RECV_MSG%: [3c] REGISTER NTFY (reg 0x1d data 05000003)
    DEBU[0181] %PHEV_TCP_SEND_MSG%: [3c] REGISTER ACK  (reg 0x1d data 00)
    DEBU[0181] %PHEV_TCP_RECV_MSG%: [fd] REGISTER NTFY (reg 0x1d data 52000003)
    DEBU[0181] %PHEV_TCP_SEND_MSG%: [fd] REGISTER ACK  (reg 0x1d data 00)
    
    repeatedly in response to level request. This only happens in some
    instances but not others. Never after a fresh connection. Vehicle only
    ever self-reports in increments of 5% anyway (maybe that's what this
    means??) so lets just ignore any readings below this value after the
    first read-out of the battery level: if the battery is already below 5%,
    then the first report after a connection is going to remain roughly
    accurate.
    
    This is necessary for an integration with tools like evcc or if plotting
    the values onto a graph. Otherwise the repeated 5% inserts are going to
    make both of these use-cases really difficult to achieve.

--- cmd/mqtt.go
+++ cmd/mqtt.go
@@ -126,6 +126,7 @@ type mqttClient struct {
 
 	phev        *client.Client
 	lastConnect time.Time
+	everPublishedBatteryLevel bool
 
 	prefix string
 
@@ -349,6 +350,7 @@ func (m *mqttClient) handlePhev(cmd *cobra.Command) error {
 		return err
 	}
 	m.client.Publish(m.topic("/available"), 0, true, "online")
+	m.everPublishedBatteryLevel = false
 	defer func() {
 		m.lastConnect = time.Now()
 	}()
@@ -440,7 +442,12 @@ func (m *mqttClient) publishRegister(msg *protocol.PhevMessage) {
 		m.publish("/door/boot", boolOpen[reg.Boot])
 		m.publish("/lights/head", boolOnOff[reg.Headlights])
 	case *protocol.RegisterBatteryLevel:
-		m.publish("/battery/level", fmt.Sprintf("%d", reg.Level))
+		if !m.everPublishedBatteryLevel || reg.Level > 5 {
+			m.everPublishedBatteryLevel = true
+			m.publish("/battery/level", fmt.Sprintf("%d", reg.Level))
+		} else {
+			log.Debugf("Ignoring battery level reading: %v", reg.Level)
+		}
 		m.publish("/lights/parking", boolOnOff[reg.ParkingLights])
 	case *protocol.RegisterChargePlug:
 		if reg.Connected {
